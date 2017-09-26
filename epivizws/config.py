2ea36a16c9975d2006e80eec69af1af8b8bb4e94
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2017-09-26 12:55:36 -0400

    Update config.py

--- epivizws/config.py
+++ epivizws/config.py
@@ -13,7 +13,7 @@ class Config(object):
     CSRF_ENABLED = True
     THREADED = True
     SECRET = "BJbfSFrdtfNIxE5F0j5a6tGcV1ghAH"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:K@ncherl@88@localhost/epiviz"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:<KEY>@localhost/epiviz"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app_config = Config
